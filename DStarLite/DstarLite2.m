bd83c7a0148d2dfd03fe79e633d723a6ff5c95c1
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2021-12-15 18:28:45 +0100

    code modified
    modified to use PriorityQueue

--- DStarLite/DstarLite2.m
+++ DStarLite/DstarLite2.m
@@ -4,7 +4,7 @@ clc
 %% Main
 
 global dim Sstart Sgoal globalObstacles robotLocVal obstacleVal goalVal unknownVal moves g
-global map obstacles newObstacles
+global map obstacles newObstacles U g rhs
 
 goalVal = 2;
 robotLocVal = 5;
@@ -12,15 +12,15 @@ unknownVal = 7;
 obstacleVal = 10;
 
 dim = [5, 3];
-Sstart = [1, 1];
-Sgoal = [5, 3];
+Sstart = [1; 1];
+Sgoal = [5; 3];
 
-globalObstacles = [[2, 2]; [3, 2]; [4, 2]]';
+globalObstacles = [[2; 2], [3; 2], [4; 2]];
 
 obstacles = [];
 newObstacles = [];
 
-moves = [[1, 0]; [1, 1]; [0, 1]; [-1, 1]; [-1, 0]; [-1, -1]; [0, -1]; [1, -1]]';
+moves = [[1; 0], [1; 1], [0; 1], [-1; 1], [-1; 0], [-1; -1], [0; -1], [1; -1]];
 
 Initialize();
 
@@ -43,29 +43,29 @@ while(any(Sstart ~= Sgoal))
     end
     
     minV = +inf;
-    minPos = [-1, -1];
+    minPos = [-1; -1];
     succ = u_succ(Sstart);
     for s=succ
         [is, js] = in(s');
         curr = c(Sstart, s') + g(is, js);
         if curr < minV
             minV = curr;
-            minPos = [is, js];
+            minPos = [is; js];
         end
     end
     
     %move to minPos
     Sstart = minPos;
     
-    % scan graph
-    isChanged = updateMap();
-    
-    % update graph
-    if isChanged
-        updateEdgesCost();
-%         Initialize();
-        ComputeShortestPath();
-    end
+%     % scan graph
+%     isChanged = updateMap();
+%     
+%     % update graph
+%     if isChanged
+%         updateEdgesCost();
+% %         Initialize();
+%         ComputeShortestPath();
+%     end
     
     plotMap(map);
     disp(g)
@@ -124,7 +124,7 @@ function isChanged = updateMap()
 end
 
 function plotMap(map)
-    global dim robotLocVal obstacleVal goalVal unknownVal Sstart Sgoal
+    global dim obstacleVal unknownVal Sstart Sgoal
     %myMap = [1 1 1; 1 0 0; 0 1 0; 0 0 1; 0 0 0];
     %heatmap(map, 'Colormap', myMap);
     [s1, s2] = in(dim);
@@ -170,73 +170,16 @@ function res = c(s1, s2)
 end
 
 function [i, j] = in(s)
+    % return s = [a, b] as 2 different values
+    % is equal to do i = s(1); j = s(2); <==> [i, j] = in(s)
+    
     i = s(1);
     j = s(2);
 end
 
-function res = min2(v1, v2)
-    if (v1(1) < v2(1))
-        res = true;
-    elseif (v1(1) == v2(1))
-        if (v1(2) < v2(2))
-            res = true;
-        elseif (v1(2) == v2(2))
-            res = false;
-        else % (v1(2) > v2(2))
-            res = false;
-        end
-    else % (v1(1) > v2(1))
-        res = false;
-    end
-end
-
-function minV = U_topKey()
-    global U
-    
-    minV = [];
-    [s1, s2] = size(U);
-    for i=1:s1
-        for j=1:s2
-            if ~isempty(U{i, j})
-                if isempty(minV) || min2(U{i, j}, minV)
-                    minV = U{i, j};
-                end
-            end
-        end
-    end
-end
-
-function minU = U_top()
-    global U
-    
-    minV = [];
-    minU = [];
-    [s1, s2] = size(U);
-    for i=1:s1
-        for j=1:s2
-            if ~isempty(U{i, j})
-                if isempty(minV) || min2(U{i, j}, minV)
-                    minV = U{i, j};
-                    minU = [i, j];
-                end
-            end
-        end
-    end
-end
-
-function U_remove(u)
-    global U
-
-    [i, j] = in(u);
-    U(i, j) = {double.empty(0)};
-end
-
-function u = U_pop()
-    u = U_top();
-    U_remove(u);
-end
-
 function res = isInside(s)
+    % check if s is inside the map
+    
     global dim
     [x, y] = in(s);
     
@@ -252,6 +195,8 @@ function res = isInside(s)
 end
 
 function isIn = isAlredyIn(L, val)
+    % check if val is inside list L
+    
     isIn = false;
     for elem=L
         if all(elem==val)
@@ -266,7 +211,7 @@ function Lp = u_pred(u)
     
     Lp = [];
     for m=moves
-        pred_pos = u+m';
+        pred_pos = u+m;
         
         %se dentro i bordi
         if ~isInside(pred_pos)
@@ -283,8 +228,8 @@ function Lp = u_pred(u)
         
         if isNotObs
             % TODO
-            if ~isAlredyIn(Lp, pred_pos')
-                Lp(:, end+1) = pred_pos';
+            if ~isAlredyIn(Lp, pred_pos)
+                Lp(:, end+1) = pred_pos;
             end
         end
     end
@@ -295,7 +240,7 @@ function Ls = u_succ(u)
     
     Ls = [];
     for m=moves
-        succ_pos = u+m';
+        succ_pos = u+m;
         
         %se dentro i bordi
         if ~isInside(succ_pos)
@@ -312,8 +257,8 @@ function Ls = u_succ(u)
         
         if isNotObs
             % TODO
-            if ~isAlredyIn(Ls, succ_pos')
-                Ls(:, end+1) = succ_pos';
+            if ~isAlredyIn(Ls, succ_pos)
+                Ls(:, end+1) = succ_pos;
             end
         end
     end
@@ -329,13 +274,6 @@ function K = calcKey(s)
     K = [k1, k2];
 end
 
-function res = belong2U(u)
-    global U
-
-    [i, j] = in(u);
-    res = ~isempty(cell2mat(U(i, j)));
-end
-
 function updateVertex(u)
     global Sgoal U g rhs
     
@@ -345,8 +283,8 @@ function updateVertex(u)
         minV = +inf;
         succ = u_succ(u);
         for s=succ
-            [is, js] = in(s');
-        	curr = c(u, s') + g(is, js);
+            [is, js] = in(s);
+        	curr = c(u, s) + g(is, js);
             if curr < minV
                 minV = curr;
             end
@@ -354,12 +292,12 @@ function updateVertex(u)
         rhs(i, j) = minV;
     end
     
-    if belong2U(u)
-        U_remove(u);
+    if U.has(u)
+        U = U.remove(u);
     end
     
     if g(i, j) ~= rhs(i, j)
-        U{i, j} = calcKey(u);
+        U = U.update(u, calcKey(u));
     end
 end
 
@@ -367,37 +305,35 @@ function Initialize()
     global dim g rhs U Sgoal
     g = Inf(dim);
     rhs = Inf(dim);
-    U = cell(dim);
+    U = PriorityQueue();
     
     [i, j] = in(Sgoal);
     
     rhs(i, j) = 0;
-    U{i, j} = calcKey(Sgoal);
+    U = U.insert([i, j], calcKey(Sgoal));
 end
 
 function ComputeShortestPath()
-    global Sstart g rhs
+    global Sstart g rhs U
     
     [i, j] = in(Sstart);
     
-    while (min2(U_topKey(), calcKey(Sstart)) || ...
+    while (min2(U.topKey(), calcKey(Sstart)) || ...
             rhs(i, j) ~= g(i, j))
-        u = U_pop();
+        [U, u] = U.pop();
         [ui, uj] = in(u);
         
         if (g(ui, uj) > rhs(ui, uj))
             g(ui, uj) = rhs(ui, uj);
-            pred = u_pred(u);
-            for k=1:length(pred)
-                s = pred(:, k)';
-                updateVertex(s);
-            end
         else
             g(ui, uj) = +inf;
             updateVertex(u);
-            %for s in pred(u)
-            %    updateVertex(s);
-            %end
+        end
+        
+        pred = u_pred(u);
+        for k=1:length(pred)
+            s = pred(:, k);
+            updateVertex(s);
         end
     end
 end

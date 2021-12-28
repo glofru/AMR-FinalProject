340c9c53685e40ff6b92288dae060ccdfce663a6
Denise Landini <79659258+deniselandini@users.noreply.github.com>
2021-12-28 23:33:34 +0100

    Add files via upload

---
+++ Differential-Drive-Robot-main/matlab_simulation/planners/DStarLite/planning_fun_D_star_lite_v2.m
@@ -0,0 +1,9 @@
+function [final_path] = planning_fun_D_star_lite_v2(state_robot,dt,limit,goal,image,resolution,maxIter)
+    grid_search = D_star_lite_v2(state_robot,dt,limit,goal,image,resolution,maxIter);
+    
+    grid_search.computeShortestPath();
+    
+    final_path = grid_search.run();
+end
+
+

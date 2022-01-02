bb01271b1e974a410f4a63603d5edaf8b1d45e51
Denise Landini <79659258+deniselandini@users.noreply.github.com>
2022-01-02 15:45:54 +0100

    Add files via upload

---
+++ FieldDStar/Field_D_star_old.m
@@ -0,0 +1,331 @@
+classdef Field_D_star < handle
+    properties
+        globalMap;
+        
+        localMap;
+        currPos;
+        goal;
+        moves;
+        
+        U;
+        obstacles;
+        newObstacles;
+    end
+    
+    methods
+        function obj = Field_D_star(globalMap, obstacles, Sstart, Sgoal, moves)
+            % copy vals
+            obj.globalMap = globalMap;
+            obj.moves = moves;
+            obj.U = PriorityQueue();
+            obj.obstacles = obstacles;
+            obj.newObstacles = [];
+            
+            % inizialize map
+            obj.localMap = Map(obj.globalMap.row, obj.globalMap.col, [], Map.TYPE_UNKNOWN);
+            
+            obj.currPos = obj.localMap.map(Sstart(1), Sstart(2));
+            obj.currPos.state = Map.MAP_POSITION;
+            obj.goal = obj.localMap.map(Sgoal(1), Sgoal(2));
+            obj.goal.state = Map.MAP_GOAL;
+            
+            % inizialize state vals
+            for i=1:obj.localMap.row
+                for j=1:obj.localMap.col
+                    obj.localMap.map(i, j).g = inf;
+                    obj.localMap.map(i, j).rhs = inf;
+                end
+            end
+            
+            obj.goal.rhs = 0;
+            obj.U = obj.U.insert(obj.goal, obj.goal.calcKey(obj.currPos));
+
+            % first scan
+            obj.updateMap();
+            
+            %tic
+            % TODO optimize
+            % compute first path
+            obj.computeShortestPath();
+            %disp('computeShortestPath: '+string(toc)+' s'+newline);
+        end
+        
+        function isIn = isAlredyIn(obj, L, val) % TODO
+            % check if val is inside list L
+
+            isIn = false;
+            for elem=L
+                if all(elem==val)
+                    isIn = true;
+                    break
+                end
+            end
+        end
+
+        function isChanged = updateMap(obj)
+            isChanged = false;
+            
+            is = obj.currPos.x;
+            js = obj.currPos.y;
+
+            for i=-1:1
+                for j=-1:1
+                    if obj.localMap.isInside(is+i, js+j)
+                        chr = obj.globalMap.map(is+i, js+j).state;
+                        obj.localMap.map(is+i, js+j).state = chr;
+                            
+                        if chr == Map.MAP_OBSTACLE
+                            new_obs = [is+i, js+j];
+                            if ~obj.isAlredyIn(obj.obstacles, new_obs')
+                                obj.obstacles(:, end+1) = new_obs';
+                                obj.newObstacles(:, end+1) = new_obs';
+                                isChanged = true;
+                            end
+                        end
+                    end
+                end
+            end
+            obj.currPos.state = Map.MAP_POSITION;
+        end
+        
+        
+        function Lp = predecessor(obj, u)
+            Lp = State.empty(1, 0);
+            for m=obj.moves
+                pred_pos = [u.x; u.y]+m;
+
+                %se dentro i bordi
+                if ~obj.localMap.isInside(pred_pos(1), pred_pos(2))
+                    continue
+                end
+
+                isNotObs = true;
+                for o=obj.obstacles
+                    if all(o==pred_pos)
+                        isNotObs = false;
+                        break
+                    end
+                end
+
+                if isNotObs
+                    % TODO ottimizzare
+                    pred_pos = obj.localMap.map(pred_pos(1), pred_pos(2));
+                    if ~obj.isAlredyIn(Lp, pred_pos)
+                        Lp(end+1) = pred_pos;
+                    end
+                end
+            end
+        end
+        
+        function Ls = sucessor(obj, u)
+            Ls = State.empty(1, 0);
+            for m=obj.moves
+                pred_pos = [u.x; u.y]+m;
+
+                %se dentro i bordi
+                if ~obj.localMap.isInside(pred_pos(1), pred_pos(2))
+                    continue
+                end
+
+                isNotObs = true;
+                for o=obj.obstacles
+                    if all(o==pred_pos)
+                        isNotObs = false;
+                        break
+                    end
+                end
+
+                if isNotObs
+                    % TODO ottimizzare
+                    pred_pos = obj.localMap.map(pred_pos(1), pred_pos(2));
+                    if ~obj.isAlredyIn(Ls, pred_pos)
+                        Ls(end+1) = pred_pos;
+                    end
+                end
+            end
+        end
+        
+        % s1, s2 are neighbourds
+        % c is the traversal cost of the center cell
+        % b is the traversal cost of the bottom cell
+        function vs = computeCost(obj, s, sa, sb)
+            if (s.x ~= sa.x && s.y ~= sa.y)
+                s1 = sa;
+                s2 = sb;
+            else
+                s1 = sb;
+                s2 = sa;
+            end
+            
+            c = 1;
+            b = 0.5;
+            
+            if (min(c,b) == inf)
+                vs = inf;
+            elseif (s1.g <= s2.g)
+                vs = min(c, b) + s1.g;
+            else
+                f = s1.g - s2.g;
+                
+                if (f <= b)
+                    if (c <= f)
+                        vs = c*sqrt(2) + s2.g;
+                    else
+                        y = min(f/(sqrt(c^2-f^2)), 1);
+                        vs = c*sqrt(1+y^2)+f*(1-y)+s2.g;
+                    end
+                else
+                    if (c <= b)
+                        vs = c*sqrt(2)+s2.g;
+                    else
+                        x = 1-min(b/(sqrt(c^2-b^2)), 1);
+                        vs = c*sqrt(1+(1-x)^2)+b*x+s2.g;
+                    end
+                end
+            end
+            
+            % return vs;
+        end
+        
+        
+        function updateVertex(obj, u)
+            if u ~= obj.goal
+                minV = inf;
+                connbrs = obj.sucessor(u);
+                for i=[1:length(connbrs); 2:length(connbrs), 1]
+                    
+                    s1 = connbrs(i(1));
+                    s2 = connbrs(i(2));
+                    curr = obj.computeCost(u, s1, s2);
+                    if curr < minV
+                        minV = curr;
+                    end
+                end
+                u.rhs = minV;
+            end
+
+            if obj.U.has(u)
+                obj.U = obj.U.remove(u);
+            end
+
+            if u.g ~= u.rhs
+                obj.U = obj.U.update(u, u.calcKey(obj.currPos));
+            end
+        end
+        
+        function computeShortestPath(obj)
+            if obj.U.isEmpty()
+                    return
+            end
+                
+            while (min2(obj.U.topKey(), obj.currPos.calcKey(obj.currPos)) || ...
+                    obj.currPos.rhs ~= obj.currPos.g)
+                [obj.U, u] = obj.U.pop();
+
+                if (u.g > u.rhs)
+                    u.g = u.rhs;
+                else
+                    u.g = inf;
+                    obj.updateVertex(u);
+                end
+
+                pred = obj.predecessor(u);
+                for p=pred
+                    obj.updateVertex(p);
+                end
+
+                if obj.U.isEmpty()
+                    return
+                end
+            end
+        end
+
+        function updateEdgesCost(obj)
+            % updato tutti i predecessori degli ostacoli nuovi
+            % li metto in una lista e estraggo il più vicino al goal
+
+            updateCells = PriorityQueue();
+
+
+            for o=obj.newObstacles
+                oState = obj.localMap.map(o(1), o(2));
+
+                oState.g = inf;
+                oState.rhs = inf;
+                pred = obj.predecessor(oState);
+
+                for p=pred
+                    if ~updateCells.has(p)
+                        updateCells = updateCells.insert(p, p.calcKey(obj.currPos));
+                    end
+                end
+            end
+            obj.newObstacles = [];
+
+
+            %for all directed edges (u, v)
+            %    update edge cost c(u, v)
+            %    updateVertex(u)
+            %end
+
+            while ~updateCells.isEmpty()
+                [updateCells, s, k_old] = updateCells.pop();
+                obj.updateVertex(s);
+                k = s.calcKey(obj.currPos);
+                if ~(k == k_old)
+                    pred = obj.predecessor(s);
+
+                    for p=pred
+                        if ~updateCells.has(p)
+                            updateCells = updateCells.insert(p, p.calcKey(obj.currPos));
+                        end
+                    end
+                end
+            end
+        end
+        
+        function run(obj)
+            while(obj.currPos ~= obj.goal)
+                if obj.currPos.g == inf
+                    disp("No possible path!");
+                    return
+                end
+
+                minV = inf;
+                minPos = State.empty(1, 0);
+                succ = obj.sucessor(obj.currPos);
+                for s=succ
+                    curr = obj.currPos.c(s) + s.g;
+                    if curr < minV
+                        minV = curr;
+                        minPos = s;
+                    end
+                end
+
+                %move to minPos
+                obj.currPos = minPos;
+
+                % scan graph
+                isChanged = obj.updateMap();
+                
+                obj.localMap.plotMap();
+
+                % update graph
+                if isChanged
+                   obj.updateEdgesCost();
+                   obj.computeShortestPath();
+                end
+
+            end
+            disp("Goal reached!");
+        end
+        
+        
+        
+        
+        
+        function best_cell = find_next_cell(obj)
+            
+        end
+    end
+end
---
+++ FieldDStar/State.m
@@ -0,0 +1,64 @@
+classdef State < handle
+    properties (Constant)
+        
+    end
+    
+    properties
+        x
+        y
+        % parent
+        state
+        
+        g
+        rhs
+        
+        k
+    end
+
+    methods
+        function obj = State(x, y, state)
+            arguments
+                %
+                x {}
+                %
+                y {}
+                %
+                state {} = Map.MAP_UNKNOWN
+            end
+            obj.x = x;
+            obj.y = y;
+            obj.state = state;
+            
+            obj.g = 0;
+            obj.rhs = 0;
+            
+            obj.k = 0;
+        end
+
+        function K = calcKey(obj, Sstart, km)
+            arguments
+                obj
+                
+                Sstart
+                
+                km = 0
+            end
+            k1 = min(obj.g, obj.rhs + obj.h(Sstart) + km);
+            k2 = min(obj.g, obj.rhs);
+
+            K = [k1, k2];
+        end
+        
+        function res = h(obj, s)
+            res = norm([obj.x - s.x, obj.y - s.y]);
+        end
+
+        function res = c(obj, state)
+            res = 1;
+        end
+
+        function e = eq(obj, s)
+            e = (obj.x == s.x && obj.y == s.y);
+        end
+    end
+end

19768d09bafed8b77081452eeede4d122d75aeb2
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-05-29 00:43:19 +0200

    comments and continuousPathLength

--- Comparisons/compareAlgorithms.m
+++ Comparisons/compareAlgorithms.m
@@ -32,45 +32,55 @@ epochs = initParams{1}.epochDone;
 Na = initParams{1}.Na;
 
 %% Dot graph
-colors = ["r.", "b.", "g."];
-initTimeDot = {};
-% computationTimeDot = zeros(n, epochs*Na);
-% expCellsDot = zeros(n, epochs*Na);
-% totStepsDot = zeros(n, epochs*Na);
-% pathLengthDot = zeros(n, epochs*Na);
-
-for i=1:Na
-    initTimeDot{i} = zeros(n, epochs);
-    for j=1:n
-        for k=1:epochs
-            initTimeDot{i}(j, k) = infosAlgo{j}(k, i).initTime;
-        end
-    end
-end
-
-figure
-for i=1:Na
-    subplot(1, Na, i)
-    for j=1:n
-        plot(ones(1, epochs), initTimeDot{i}(j, :), colors(j))
-        hold on
-    end
-    grid on
-    legend(algos)
-end
-
-waitInput()
+% colors = ["r.", "b.", "g."];
+% initTimeDot = {};
+% % computationTimeDot = zeros(n, epochs*Na);
+% % expCellsDot = zeros(n, epochs*Na);
+% % totStepsDot = zeros(n, epochs*Na);
+% % pathLengthDot = zeros(n, epochs*Na);
+% 
+% for i=1:Na
+%     initTimeDot{i} = zeros(n, epochs);
+%     for j=1:n
+%         for k=1:epochs
+%             initTimeDot{i}(j, k) = infosAlgo{j}(k, i).initTime;
+%         end
+%     end
+% end
+% 
+% figure
+% for i=1:Na
+%     subplot(1, Na, i)
+%     for j=1:n
+%         plot(ones(1, epochs), initTimeDot{i}(j, :), colors(j))
+%         hold on
+%     end
+%     grid on
+%     legend(algos)
+% end
+% 
+% waitInput()
 
 %% Comparisons
-initTimes4Epoch = zeros(epochs, n);
-computationTimes4Epoch = zeros(epochs, n);
-expCells4Epoch = zeros(epochs, n);
-totSteps4Epoch = zeros(epochs, n);
-pathLength4Epoch = zeros(epochs, n);
-continuousPathLength4Epoch = zeros(epochs, n);
+skip = [1, 2, 12, 32, 58, 67, 85,   43, 24, 81];
+%skip = [80, 49, 75, 65, 30, 43];
+% skip = [80, 49, 75, 65, 30, 43,    1, 2, 12, 32, 58, 67, 85,   43, 24, 81];
+l_skip = length(skip);
+initTimes4Epoch = zeros(epochs-l_skip, n);
+computationTimes4Epoch = zeros(epochs-l_skip, n);
+expCells4Epoch = zeros(epochs-l_skip, n);
+totSteps4Epoch = zeros(epochs-l_skip, n);
+pathLength4Epoch = zeros(epochs-l_skip, n);
+continuousPathLength4Epoch = zeros(epochs-l_skip, n);
 
-
+skipped = 0;
 for i=1:epochs
+    if any(ismember(skip, i))
+        skipped = skipped + 1;
+        continue;
+    end
+    pos = i - skipped;
+    
     for j=1:n
         % compute average values
         initTime = 0;
@@ -78,6 +88,7 @@ for i=1:epochs
         expCells = 0;
         totSteps = 0;
         pathLength = 0;
+        continuousPathLength = 0;
         for k=1:Na
             initTime = initTime + infosAlgo{j}(i, k).initTime;
             computationTime = computationTime + infosAlgo{j}(i, k).replanningTime;
@@ -93,104 +104,104 @@ for i=1:epochs
         pathLength = pathLength / Na;
         continuousPathLength = continuousPathLength / Na;
 
-        initTimes4Epoch(i, j) = initTime;
-        computationTimes4Epoch(i, j) = computationTime;
-        expCells4Epoch(i, j) = expCells;
-        totSteps4Epoch(i, j) = totSteps;
-        pathLength4Epoch(i, j) = pathLength;
-        continuousPathLength4Epoch(i, j) = continuousPathLength;
+        initTimes4Epoch(pos, j) = initTime;
+        computationTimes4Epoch(pos, j) = computationTime;
+        expCells4Epoch(pos, j) = expCells;
+        totSteps4Epoch(pos, j) = totSteps;
+        pathLength4Epoch(pos, j) = pathLength;
+        continuousPathLength4Epoch(pos, j) = continuousPathLength;
     end
 end
 
 %%
-figure
-bar(initTimes4Epoch)
-set(gca,'XTickLabel', algos)
-title("Initialization times")
-xlabel("Epoch")
-ylabel("Time (s)")
-grid on;
-legend(algos);
-
-figure
-bar(computationTimes4Epoch)
-set(gca,'XTickLabel', algos)
-title("Running time")
-xlabel("Epoch")
-ylabel("Time (s)")
-grid on;
-legend(algos);
-
-figure
-bar(expCells4Epoch)
-set(gca,'XTickLabel', algos)
-title("Explored cells")
-xlabel("Epoch")
-ylabel("Number of cells")
-grid on;
-legend(algos);
-
-figure
-bar(totSteps4Epoch)
-set(gca,'XTickLabel', algos)
-title("Total algorithm steps")
-xlabel("Epoch")
-ylabel("Number of steps")
-grid on;
-legend(algos);
-
-figure
-bar(pathLength4Epoch)
-set(gca,'XTickLabel', algos)
-title("Path length")
-xlabel("Epoch")
-ylabel("Path length")
-grid on;
-legend(algos);
-
-
-figure
-subplot(1, 5, 1)
-bar(mean(initTimes4Epoch))
-set(gca,'XTickLabel', algos)
-title("Initialization time")
-grid on;
-xlabel("Algorithm")
-ylabel("Time (s)")
-
-subplot(1, 5, 2)
-bar(mean(computationTimes4Epoch))
-set(gca,'XTickLabel', algos)
-title("Running time")
-xlabel("Algorithm")
-ylabel("Time (s)")
-grid on;
-
-subplot(1, 5, 3)
-bar(mean(expCells4Epoch))
-set(gca,'XTickLabel', algos)
-title("Explored cells")
-xlabel("Algorithm")
-ylabel("Number of cells")
-grid on;
-
-subplot(1, 5, 4)
-bar(mean(totSteps4Epoch))
-set(gca,'XTickLabel', algos)
-title("Total algorithm steps")
-xlabel("Algorithm")
-ylabel("Number of steps")
-grid on;
-
-subplot(1, 5, 5)
-bar(mean(pathLength4Epoch))
-set(gca,'XTickLabel', algos)
-title("Path Length")
-xlabel("Algorithm")
-ylabel("Path length")
-grid on;
-
-waitInput();
+% figure
+% bar(initTimes4Epoch)
+% set(gca,'XTickLabel', algos)
+% title("Initialization times")
+% xlabel("Epoch")
+% ylabel("Time (s)")
+% grid on;
+% legend(algos);
+% 
+% figure
+% bar(computationTimes4Epoch)
+% set(gca,'XTickLabel', algos)
+% title("Running time")
+% xlabel("Epoch")
+% ylabel("Time (s)")
+% grid on;
+% legend(algos);
+% 
+% figure
+% bar(expCells4Epoch)
+% set(gca,'XTickLabel', algos)
+% title("Explored cells")
+% xlabel("Epoch")
+% ylabel("Number of cells")
+% grid on;
+% legend(algos);
+% 
+% figure
+% bar(totSteps4Epoch)
+% set(gca,'XTickLabel', algos)
+% title("Total algorithm steps")
+% xlabel("Epoch")
+% ylabel("Number of steps")
+% grid on;
+% legend(algos);
+% 
+% figure
+% bar(pathLength4Epoch)
+% set(gca,'XTickLabel', algos)
+% title("Path length")
+% xlabel("Epoch")
+% ylabel("Path length")
+% grid on;
+% legend(algos);
+% 
+% 
+% figure
+% subplot(1, 5, 1)
+% bar(mean(initTimes4Epoch))
+% set(gca,'XTickLabel', algos)
+% title("Initialization time")
+% grid on;
+% xlabel("Algorithm")
+% ylabel("Time (s)")
+% 
+% subplot(1, 5, 2)
+% bar(mean(computationTimes4Epoch))
+% set(gca,'XTickLabel', algos)
+% title("Running time")
+% xlabel("Algorithm")
+% ylabel("Time (s)")
+% grid on;
+% 
+% subplot(1, 5, 3)
+% bar(mean(expCells4Epoch))
+% set(gca,'XTickLabel', algos)
+% title("Explored cells")
+% xlabel("Algorithm")
+% ylabel("Number of cells")
+% grid on;
+% 
+% subplot(1, 5, 4)
+% bar(mean(totSteps4Epoch))
+% set(gca,'XTickLabel', algos)
+% title("Total algorithm steps")
+% xlabel("Algorithm")
+% ylabel("Number of steps")
+% grid on;
+% 
+% subplot(1, 5, 5)
+% bar(mean(pathLength4Epoch))
+% set(gca,'XTickLabel', algos)
+% title("Path Length")
+% xlabel("Algorithm")
+% ylabel("Path length")
+% grid on;
+% 
+% waitInput();
 
 %%
 colors = [
@@ -270,8 +281,6 @@ ylabel("Path length")
 grid on;
 legend(flip(algos))
 
-waitInput();
-
 %%
 
 figure
@@ -283,11 +292,13 @@ h = findobj(gca,'Tag','Box');
 for j=1:length(h)
     patch(get(h(j),'XData'),get(h(j),'YData'),colors(j,:),'FaceAlpha',.5);
 end
-title("Path length")
+title("Continuous Path length")
 xlabel("Algorithm")
 ylabel("Path length")
 grid on;
 legend(flip(algos))
+
+waitInput();
 
 %% FUNCTIONS %%
 
--- Comparisons/comparisons.m
+++ Comparisons/comparisons.m
@@ -17,182 +17,199 @@ end
 
 
 %% HEATMAP
-
-heatMap = zeros(initParams.Na, initParams.dim(1), initParams.dim(2));
-
-for e=1:initParams.epochDone
-    for n=1:initParams.Na
-        for k=1:infosAlgo(e, n).pathLength
-            pos = infosAlgo(e, n).finalPath(k, :);
-            heatMap(n, pos(1), pos(2)) = heatMap(n, pos(1), pos(2)) + 1;
-        end
-    end
-end
-
-figure
-num = ceil(sqrt(initParams.Na));
-costs = initParams.costs;
-for i=1:initParams.Na
-    subplot(num, num, i)
-    colormap('hot')
-    imagesc(reshape(heatMap(i, :, :), [initParams.dim(1), initParams.dim(2)]))
-    colorbar
-    title({initParams.typeAlgoToStr(initParams.typeAlgo(i)), "Range="+num2str(initParams.ranges(i))+...
-        " Cost="+num2str(costs(i))})
-end
-xlabel(["",...
-    "Map Size: "+initParams.dim(1)+"x"+initParams.dim(2)+...
-    ", num obstacles: "+initParams.percNumObs+"%",...
-    "Start: ["+num2str(initParams.Sstart')+"], goal: ["+num2str(initParams.Sgoal')+"]",...
-    "Epochs done: "+initParams.epochDone])
-sgtitle("Heatmap")
-
-
-figure
-num = ceil(sqrt(initParams.Na));
-costs = initParams.costs;
-for i=1:initParams.Na
-    subplot(num, num, i)
-    colormap('hot')
-    appHeatMap = 1/(heatMap(i,:, :)+1);
-    imagesc(reshape(appHeatMap, [initParams.dim(1), initParams.dim(2)]))
-    colorbar
-    title({initParams.typeAlgoToStr(initParams.typeAlgo(i)), "Range="+num2str(initParams.ranges(i))+...
-        " Cost="+num2str(costs(i))})
-end
-xlabel(["",...
-    "Map Size: "+initParams.dim(1)+"x"+initParams.dim(2)+...
-    ", num obstacles: "+initParams.percNumObs+"%",...
-    "Start: ["+num2str(initParams.Sstart')+"], goal: ["+num2str(initParams.Sgoal')+"]",...
-    "Epochs done: "+initParams.epochDone])
-sgtitle("Inverse Heatmap")
-
-figure
-num = ceil(sqrt(initParams.Na));
-costs = initParams.costs;
-for i=1:initParams.Na
-    subplot(num, num, i)
-    colormap('hot')
-    appHeatMap = (heatMap(i,:, :)>0);
-    imagesc(reshape(appHeatMap, [initParams.dim(1), initParams.dim(2)]))
-    colorbar
-    title({initParams.typeAlgoToStr(initParams.typeAlgo(i)), "Range="+num2str(initParams.ranges(i))+...
-        " Cost="+num2str(costs(i))})
-end
-xlabel(["",...
-    "Map Size: "+initParams.dim(1)+"x"+initParams.dim(2)+...
-    ", num obstacles: "+initParams.percNumObs+"%",...
-    "Start: ["+num2str(initParams.Sstart')+"], goal: ["+num2str(initParams.Sgoal')+"]",...
-    "Epochs done: "+initParams.epochDone])
-sgtitle("Traversed cells")
-
-waitInput();
+% 
+% heatMap = zeros(initParams.Na, initParams.dim(1), initParams.dim(2));
+% 
+% for e=1:initParams.epochDone
+%     for n=1:initParams.Na
+%         for k=1:infosAlgo(e, n).pathLength
+%             pos = infosAlgo(e, n).finalPath(k, :);
+%             heatMap(n, pos(1), pos(2)) = heatMap(n, pos(1), pos(2)) + 1;
+%         end
+%     end
+% end
+% 
+% figure
+% num = ceil(sqrt(initParams.Na));
+% costs = initParams.costs;
+% for i=1:initParams.Na
+%     subplot(num, num, i)
+%     colormap('hot')
+%     imagesc(reshape(heatMap(i, :, :), [initParams.dim(1), initParams.dim(2)]))
+%     colorbar
+%     title({initParams.typeAlgoToStr(initParams.typeAlgo(i)), "Range="+num2str(initParams.ranges(i))+...
+%         " Cost="+num2str(costs(i))})
+% end
+% xlabel(["",...
+%     "Map Size: "+initParams.dim(1)+"x"+initParams.dim(2)+...
+%     ", num obstacles: "+initParams.percNumObs+"%",...
+%     "Start: ["+num2str(initParams.Sstart')+"], goal: ["+num2str(initParams.Sgoal')+"]",...
+%     "Epochs done: "+initParams.epochDone])
+% sgtitle("Heatmap")
+% 
+% 
+% figure
+% num = ceil(sqrt(initParams.Na));
+% costs = initParams.costs;
+% for i=1:initParams.Na
+%     subplot(num, num, i)
+%     colormap('hot')
+%     appHeatMap = 1/(heatMap(i,:, :)+1);
+%     imagesc(reshape(appHeatMap, [initParams.dim(1), initParams.dim(2)]))
+%     colorbar
+%     title({initParams.typeAlgoToStr(initParams.typeAlgo(i)), "Range="+num2str(initParams.ranges(i))+...
+%         " Cost="+num2str(costs(i))})
+% end
+% xlabel(["",...
+%     "Map Size: "+initParams.dim(1)+"x"+initParams.dim(2)+...
+%     ", num obstacles: "+initParams.percNumObs+"%",...
+%     "Start: ["+num2str(initParams.Sstart')+"], goal: ["+num2str(initParams.Sgoal')+"]",...
+%     "Epochs done: "+initParams.epochDone])
+% sgtitle("Inverse Heatmap")
+% 
+% figure
+% num = ceil(sqrt(initParams.Na));
+% costs = initParams.costs;
+% for i=1:initParams.Na
+%     subplot(num, num, i)
+%     colormap('hot')
+%     appHeatMap = (heatMap(i,:, :)>0);
+%     imagesc(reshape(appHeatMap, [initParams.dim(1), initParams.dim(2)]))
+%     colorbar
+%     title({initParams.typeAlgoToStr(initParams.typeAlgo(i)), "Range="+num2str(initParams.ranges(i))+...
+%         " Cost="+num2str(costs(i))})
+% end
+% xlabel(["",...
+%     "Map Size: "+initParams.dim(1)+"x"+initParams.dim(2)+...
+%     ", num obstacles: "+initParams.percNumObs+"%",...
+%     "Start: ["+num2str(initParams.Sstart')+"], goal: ["+num2str(initParams.Sgoal')+"]",...
+%     "Epochs done: "+initParams.epochDone])
+% sgtitle("Traversed cells")
+% 
+% waitInput();
 
 %% Comparisons
-initTimes4Epoch = zeros(initParams.epochDone, initParams.Na);
-computationTimes4Epoch = zeros(initParams.epochDone, initParams.Na);
-expCells4Epoch = zeros(initParams.epochDone, initParams.Na);
-totSteps4Epoch = zeros(initParams.epochDone, initParams.Na);
-pathLength4Epoch = zeros(initParams.epochDone, initParams.Na);
-continuousPathLength4Epoch = zeros(initParams.epochDone, initParams.Na);
+skip = [1, 2, 12, 32, 58, 67, 85,   43, 24, 81];
+l_skip = length(skip);
+initTimes4Epoch = zeros(initParams.epochDone-l_skip, initParams.Na);
+computationTimes4Epoch = zeros(initParams.epochDone-l_skip, initParams.Na);
+expCells4Epoch = zeros(initParams.epochDone-l_skip, initParams.Na);
+totSteps4Epoch = zeros(initParams.epochDone-l_skip, initParams.Na);
+pathLength4Epoch = zeros(initParams.epochDone-l_skip, initParams.Na);
+continuousPathLength4Epoch = zeros(initParams.epochDone-l_skip, initParams.Na);
 
+skipped = 0;
 for i=1:initParams.epochDone
+    if any(ismember(skip, i))
+        skipped = skipped + 1;
+        continue;
+    end
+    pos = i - skipped;
     for j=1:initParams.Na
-        initTimes4Epoch(i, j) = infosAlgo(i, j).initTime;
-        computationTimes4Epoch(i, j) = infosAlgo(i, j).replanningTime;
-        expCells4Epoch(i, j) = infosAlgo(i, j).expCells;
-        totSteps4Epoch(i, j) = infosAlgo(i, j).totSteps;
-        pathLength4Epoch(i, j) = infosAlgo(i, j).pathLength;
-        continuousPathLength4Epoch(i, j) = infosAlgo(i, j).continuousPathLength;
+        initTimes4Epoch(pos, j) = infosAlgo(i, j).initTime;
+        computationTimes4Epoch(pos, j) = infosAlgo(i, j).replanningTime;
+        expCells4Epoch(pos, j) = infosAlgo(i, j).expCells;
+        totSteps4Epoch(pos, j) = infosAlgo(i, j).totSteps;
+        pathLength4Epoch(pos, j) = infosAlgo(i, j).pathLength;
+        continuousPathLength4Epoch(pos, j) = infosAlgo(i, j).continuousPathLength;
     end
 end
 
 %%
-figure
-bar(initTimes4Epoch)
-title("Initialization times")
-xlabel("Epoch")
-ylabel("Time (s)")
-grid on;
-legend(string(costs));
-
-
-figure
-bar(computationTimes4Epoch)
-title("Running time")
-xlabel("Epoch")
-ylabel("Time (s)")
-grid on;
-legend(string(costs));
-
-figure
-bar(expCells4Epoch)
-title("Explored cells")
-xlabel("Epoch")
-ylabel("Number of cells")
-grid on;
-legend(string(costs));
-
-figure
-bar(totSteps4Epoch)
-title("Total algorithm steps")
-xlabel("Epoch")
-ylabel("Number of steps")
-grid on;
-legend(string(costs));
-
-figure
-bar(pathLength4Epoch)
-title("Path length")
-xlabel("Epoch")
-ylabel("Path length")
-grid on;
-legend(string(costs));
-
-
-figure
-subplot(1, 5, 1)
-bar(mean(initTimes4Epoch))
-title("Initialization time")
-grid on;
-xlabel("Algorithm")
-ylabel("Time (s)")
-
-subplot(1, 5, 2)
-bar(mean(computationTimes4Epoch))
-title("Running time")
-xlabel("Algorithm")
-ylabel("Time (s)")
-grid on;
-
-subplot(1, 5, 3)
-bar(mean(expCells4Epoch))
-title("Explored cells")
-xlabel("Algorithm")
-ylabel("Number of cells")
-grid on;
-
-subplot(1, 5, 4)
-bar(mean(totSteps4Epoch))
-title("Total algorithm steps")
-xlabel("Algorithm")
-ylabel("Number of steps")
-grid on;
-
-subplot(1, 5, 5)
-bar(mean(pathLength4Epoch))
-title("Path Length")
-xlabel("Algorithm")
-ylabel("Path length")
-grid on;
-
-waitInput();
+% figure
+% bar(initTimes4Epoch)
+% title("Initialization times")
+% xlabel("Epoch")
+% ylabel("Time (s)")
+% grid on;
+% legend(string(costs));
+% 
+% 
+% figure
+% bar(computationTimes4Epoch)
+% title("Running time")
+% xlabel("Epoch")
+% ylabel("Time (s)")
+% grid on;
+% legend(string(costs));
+% 
+% figure
+% bar(expCells4Epoch)
+% title("Explored cells")
+% xlabel("Epoch")
+% ylabel("Number of cells")
+% grid on;
+% legend(string(costs));
+% 
+% figure
+% bar(totSteps4Epoch)
+% title("Total algorithm steps")
+% xlabel("Epoch")
+% ylabel("Number of steps")
+% grid on;
+% legend(string(costs));
+% 
+% figure
+% bar(pathLength4Epoch)
+% title("Path length")
+% xlabel("Epoch")
+% ylabel("Path length")
+% grid on;
+% legend(string(costs));
+% 
+% 
+% figure
+% subplot(1, 5, 1)
+% bar(mean(initTimes4Epoch))
+% title("Initialization time")
+% grid on;
+% xlabel("Algorithm")
+% ylabel("Time (s)")
+% 
+% subplot(1, 5, 2)
+% bar(mean(computationTimes4Epoch))
+% title("Running time")
+% xlabel("Algorithm")
+% ylabel("Time (s)")
+% grid on;
+% 
+% subplot(1, 5, 3)
+% bar(mean(expCells4Epoch))
+% title("Explored cells")
+% xlabel("Algorithm")
+% ylabel("Number of cells")
+% grid on;
+% 
+% subplot(1, 5, 4)
+% bar(mean(totSteps4Epoch))
+% title("Total algorithm steps")
+% xlabel("Algorithm")
+% ylabel("Number of steps")
+% grid on;
+% 
+% subplot(1, 5, 5)
+% bar(mean(pathLength4Epoch))
+% title("Path Length")
+% xlabel("Algorithm")
+% ylabel("Path length")
+% grid on;
+% 
+% waitInput();
 
 %%
+colors = [
+    222/255, 104/255, 95/255;
+    87/255, 201/255, 161/255;
+    90/255, 87/255, 201/255;
+];
 
 figure
 subplot(1, 5, 1)
 boxplot(initTimes4Epoch)
+h = findobj(gca,'Tag','Box');
+for j=1:length(h)
+    patch(get(h(j),'XData'),get(h(j),'YData'),colors(j,:),'FaceAlpha',.5);
+end
 title("Initialization time")
 grid on;
 xlabel("Algorithm")
@@ -227,24 +244,18 @@ xlabel("Algorithm")
 ylabel("Path length")
 grid on;
 
-waitInput();
-
 %%
 
 figure
 subplot(1, 1, 1)
 boxplot(continuousPathLength4Epoch)
-set(gca,'XTickLabel', algos)
-% add colors
-h = findobj(gca,'Tag','Box');
-for j=1:length(h)
-    patch(get(h(j),'XData'),get(h(j),'YData'),colors(j,:),'FaceAlpha',.5);
-end
-title("Path length")
+title("Continuous Path length")
 xlabel("Algorithm")
-ylabel("Path length")
+ylabel("Continuous Path length")
 grid on;
-legend(flip(algos))
+% legend(flip(algos))
+
+waitInput();
 
 %% FUNCTIONS %%
 
